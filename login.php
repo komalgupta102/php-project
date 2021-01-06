7ab80e0ad74aa90832e6c4b903b79016a99aea89
Komal Gupta <komal.gupta102@gmail.com>
2021-01-06 18:27:33 +0530

    fb login

--- login.php
+++ login.php
@@ -2,70 +2,11 @@
 include_once 'helpers/init.php'; 
 include_once('config.php');
 
-//require 'vendor/autoload.php';
-//session_start();
-$fb = $facebook;
-
 if((isset($_SESSION['user_id']) && $_SESSION['user_id'] != '') || $_SESSION['fb_id']){
-  header("location:admin1");
+  header("location:admin");
 }
 
-// $facebook_output = '';
-
-// $facebook_helper = $facebook->getRedirectLoginHelper();
-
-// if(isset($_GET['code']))
-// {
-//  if(isset($_SESSION['access_token']))
-//  {
-//   $access_token = $_SESSION['access_token'];
-//  }
-//  else
-//  {
-//   $access_token = $facebook_helper->getAccessToken();
-
-//   $_SESSION['access_token'] = $access_token;
-
-//   $facebook->setDefaultAccessToken($_SESSION['access_token']);
-//  }
-
-//  $_SESSION['user_id'] = '';
-//  $_SESSION['user_name'] = '';
-//  $_SESSION['user_email_address'] = '';
-//  $_SESSION['user_image'] = '';
-
-//  $graph_response = $facebook->get("/me?fields=name,email", $access_token);
-
-//  $facebook_user_info = $graph_response->getGraphUser();
-
-//  if(!empty($facebook_user_info['id']))
-//  {
-//   $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
-//  }
-
-//  if(!empty($facebook_user_info['name']))
-//  {
-//   $_SESSION['user_name'] = $facebook_user_info['name'];
-//  }
-
-//  if(!empty($facebook_user_info['email']))
-//  {
-//   $_SESSION['user_email_address'] = $facebook_user_info['email'];
-//  }
- 
-// }
-// else
-// {
-//  // Get login url
-//     $facebook_permissions = ['email']; // Optional permissions
-
-//     $facebook_login_url = $facebook_helper->getLoginUrl($base_url.'/login.php', $facebook_permissions);
-    
-//     // Render Facebook login button
-//     $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="php-login-with-facebook.gif" /></a></div>';
-// }
-
-
+$fb = $facebook;
 $helper = $fb->getRedirectLoginHelper();
 $_SESSION['FBRLH_state']=$_GET['state'];
 $permissions = ['email']; // optional
@@ -101,7 +42,7 @@ $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 }
 // redirect the user to the profile page if it has "code" GET variable
 if (isset($_GET['code'])) {
-  header("location:admin2");
+  header("location:admin"); exit;
  // echo "<pre>"; print_r($_SESSION); exit;
 }
 // getting basic info about user
