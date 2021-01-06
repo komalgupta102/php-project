9cde0b284928391d04709907707eb4f5444d3ea1
Komal Gupta <komal.gupta102@gmail.com>
2021-01-06 13:36:07 +0530

    fb login

--- login.php
+++ login.php
@@ -3,60 +3,60 @@ include_once 'helpers/init.php';
 include_once('config.php');
 session_destroy(); //destroy the session
 
-// $facebook_output = '';
+$facebook_output = '';
 
-// $facebook_helper = $facebook->getRedirectLoginHelper();
+$facebook_helper = $facebook->getRedirectLoginHelper();
 
-// if(isset($_GET['code']))
-// {
-//  if(isset($_SESSION['access_token']))
-//  {
-//   $access_token = $_SESSION['access_token'];
-//  }
-//  else
-//  {
-//   $access_token = $facebook_helper->getAccessToken();
+if(isset($_GET['code']))
+{
+ if(isset($_SESSION['access_token']))
+ {
+  $access_token = $_SESSION['access_token'];
+ }
+ else
+ {
+  $access_token = $facebook_helper->getAccessToken();
 
-//   $_SESSION['access_token'] = $access_token;
+  $_SESSION['access_token'] = $access_token;
 
-//   $facebook->setDefaultAccessToken($_SESSION['access_token']);
-//  }
+  $facebook->setDefaultAccessToken($_SESSION['access_token']);
+ }
 
-//  $_SESSION['user_id'] = '';
-//  $_SESSION['user_name'] = '';
-//  $_SESSION['user_email_address'] = '';
-//  $_SESSION['user_image'] = '';
+ $_SESSION['user_id'] = '';
+ $_SESSION['user_name'] = '';
+ $_SESSION['user_email_address'] = '';
+ $_SESSION['user_image'] = '';
 
-//  $graph_response = $facebook->get("/me?fields=name,email", $access_token);
+ $graph_response = $facebook->get("/me?fields=name,email", $access_token);
 
-//  $facebook_user_info = $graph_response->getGraphUser();
+ $facebook_user_info = $graph_response->getGraphUser();
 
-//  if(!empty($facebook_user_info['id']))
-//  {
-//   $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
-//  }
+ if(!empty($facebook_user_info['id']))
+ {
+  $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
+ }
 
-//  if(!empty($facebook_user_info['name']))
-//  {
-//   $_SESSION['user_name'] = $facebook_user_info['name'];
-//  }
+ if(!empty($facebook_user_info['name']))
+ {
+  $_SESSION['user_name'] = $facebook_user_info['name'];
+ }
 
-//  if(!empty($facebook_user_info['email']))
-//  {
-//   $_SESSION['user_email_address'] = $facebook_user_info['email'];
-//  }
+ if(!empty($facebook_user_info['email']))
+ {
+  $_SESSION['user_email_address'] = $facebook_user_info['email'];
+ }
  
-// }
-// else
-// {
-//  // Get login url
-//     $facebook_permissions = ['email']; // Optional permissions
+}
+else
+{
+ // Get login url
+    $facebook_permissions = ['email']; // Optional permissions
 
-//     $facebook_login_url = $facebook_helper->getLoginUrl('https://greenhouses-pro.co.uk/demo/', $facebook_permissions);
+    $facebook_login_url = $facebook_helper->getLoginUrl('https://greenhouses-pro.co.uk/demo/', $facebook_permissions);
     
-//     // Render Facebook login button
-//     $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="php-login-with-facebook.gif" /></a></div>';
-// }
+    // Render Facebook login button
+    $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="php-login-with-facebook.gif" /></a></div>';
+}
 
 ?>
 <!DOCTYPE html>
@@ -90,27 +90,27 @@ session_destroy(); //destroy the session
             </div>
         </form>
     </div>
-<!--     <div class="container">
+    <div class="container">
    <br />
    <h2 align="center">PHP Login using Google Account</h2>
    <br />
-   <div class="panel panel-default"> -->
+   <div class="panel panel-default">
     <?php 
-    // if(isset($facebook_login_url))
-    // {
-    //  echo $facebook_login_url;
-    // }
-    // else
-    // {
-    //  echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
-    //  echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
-    //  echo '<h3><b>Name :</b> '.$_SESSION['user_name'].'</h3>';
-    //  echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
-    //  echo '<h3><a href="logout.php">Logout</h3></div>';
-    // }
+    if(isset($facebook_login_url))
+    {
+     echo $facebook_login_url;
+    }
+    else
+    {
+     echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
+     echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
+     echo '<h3><b>Name :</b> '.$_SESSION['user_name'].'</h3>';
+     echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
+     echo '<h3><a href="logout.php">Logout</h3></div>';
+    }
     ?>
-   <!-- </div>
-  </div> -->
+   </div>
+  </div>
 </div>
 
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
