05c65a4016d1e2e23e84fb9ec80d373987ba89fa
Komal Gupta <komal.gupta102@gmail.com>
2021-01-07 15:47:33 +0530

    sendgrid

--- helpers/sendgrid.php
+++ helpers/sendgrid.php
@@ -4,11 +4,12 @@ require '../vendor/autoload.php';
 use SendGrid\Mail\Mail;
 
 	$email = new Mail();
-	$email->setFrom("komal.gupta102@gmail.com", "Komal Gupta");
-	$email->setSubject("Registration successfull");
-	$email->addTo("komal.gupta102@gmail.com", 'Test name');
+	$email->setFrom("komal.gupta102@gmail.com", "Example User");
+	$email->setSubject("Sending with Twilio SendGrid is Fun");
+	$email->addTo("komal.gupta102@gmail.com", "Example User");
+	$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 	$email->addContent(
-	    "text/html", "<strong>Login Details are:</strong><br/>Email: "
+	    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 	);
 	$sendgrid = new \SendGrid('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');
 	try {
